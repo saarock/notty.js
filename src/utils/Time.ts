4ce8ce316b511c5742c000946817fdde6dab33ee
saarock <saarock4646@gmail.com>
2024-07-25 22:23:30 +0545

    Fix #1: Implement pause and resume toast timeout on hover

--- src/utils/Time.ts
+++ src/utils/Time.ts
@@ -1,11 +1,11 @@
 class Time {
 
-
+    // timer
      getRemainingTime(startTime: number, timeoutDelay: number): number {
         const elapsedTime = Date.now() - startTime;
          return Math.max(timeoutDelay - elapsedTime, 0);
     }
-    
+
 }
 
 const time = new Time();

cdd7e18954aef93e8aa1ba2ad89445fe1c24708a
saarock <saarock4646@gmail.com>
2024-07-26 10:48:14 +0545

    playground improve

---
+++ cypress/integration/toast.spec.ts
@@ -0,0 +1,9 @@
+describe("Toast Libary Integration Tests" , () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it ("displays a toast notification when a task is completed", () => {
+
+    })
+})

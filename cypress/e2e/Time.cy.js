2d4af3a10c11d8cbc42dd89d0d4d81a3ceac37bd
angeldevildev <mineshop47@gmail.com>
2024-07-31 22:04:39 +0200

    Added Time tests and removed NotificationMessage and NotificationToastManager that used to make errors

---
+++ cypress/e2e/Time.cy.js
@@ -0,0 +1,24 @@
+import { time } from '../../src/utils/Time';
+
+describe('Time', () => {
+  it('should return the correct remaining time', () => {
+    const startTime = Date.now();
+    const timeoutDelay = 5000; // 5 seconds
+
+    // Immediate check
+    let remainingTime = time.getRemainingTime(startTime, timeoutDelay);
+    expect(remainingTime).to.be.closeTo(timeoutDelay, 10); 
+
+    // Check after 2 seconds
+    cy.wait(2000).then(() => {
+      remainingTime = time.getRemainingTime(startTime, timeoutDelay);
+      expect(remainingTime).to.be.closeTo(timeoutDelay - 2000, 10); // 5s - 2s
+    });
+
+    // Check after 6 seconds (exceeding the timeout)
+    cy.wait(4000).then(() => {
+      remainingTime = time.getRemainingTime(startTime, timeoutDelay);
+      expect(remainingTime).to.equal(0); // Timeout exceeded
+    });
+  });
+});
